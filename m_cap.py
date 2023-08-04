5b4c155629ccbee129d1183a60292348f542a383
Ahmet Cihan <ahmedcihan@me.com>
2023-08-04 17:00:40 +0100

    get_m_cap functionality

--- m_cap.py
+++ m_cap.py
@@ -1,32 +1,40 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
-# Send a GET request to the URL
-url = "https://companiesmarketcap.com/apple/marketcap/"
-response = requests.get(url)
+def get_m_caps(name):
+    # Send a GET request to the URL
+    url = f"https://companiesmarketcap.com/{name}/marketcap/"
+    response = requests.get(url)
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the table containing the market cap data
-table = soup.find("table", {"class": "table"})
+    # Find the table containing the market cap data
+    table = soup.find("table", {"class": "table"})
 
-# Initialize empty lists to store the data
-companies = []
-market_caps = []
+    # Initialize empty lists to store the data
+    years = []
+    market_caps = []
 
-# Loop through each row in the table
-for row in table.find_all("tr")[1:]:  # Skip the header row (index 0)
-    # Extract the company name and market cap from the row
-    company = row.find("td").text.strip()
-    market_cap = row.find_all("td")[1].text.strip()
+    # Loop through each row in the table
+    for row in table.find_all("tr")[1:]:  # Skip the header row (index 0)
+        # Extract the year and market cap from the row
+        year = row.find("td").text.strip()
+        market_cap = row.find_all("td")[1].text.strip()
 
-    # Append the data to the lists
-    companies.append(company)
-    market_caps.append(market_cap)
+        # Append the data to the lists
+        years.append(year)
+        market_caps.append(market_cap)
 
-# Print the data
-for company, market_cap in zip(companies, market_caps):
-    print(f"Year: {company}, Market Cap: {market_cap}")
+    data = {
+        "Year": years,
+        "Market Cap": market_caps
+    }
+
+    df = pd.DataFrame(data)
+
+    return df
+
 
 
