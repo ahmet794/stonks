0348039441f332666ce5fa855e8ada1ae884ab25
Ahmet Cihan <ahmedcihan@me.com>
2023-08-07 23:28:47 +0100

    Portfolio Management View

---
+++ Stonker/Stonker/Portfolio Management Views/PortfolioManagementView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioManagementView.swift
+//  Stonker
+//
+//  Created by Ahmet Cihan on 07/08/2023.
+//
+
+import SwiftUI
+
+struct PortfolioManagementView: View {
+    var body: some View {
+        Text("Soon...")
+    }
+}
+
+struct PortfolioManagementView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioManagementView()
+    }
+}
